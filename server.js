095d838a876371669969a9ddcb21c19a19a05247
Harish M <your-github-harishrathnakumar10@gmail.com>
2026-06-09 09:47:56 +0530

    Fix Vercel routing

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { Resend } from "resend";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -10,6 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
